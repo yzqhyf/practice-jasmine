1ff9b16bf2135d8996c16a309a285a14f02a7738
bellcliff <bell.cliff@gmail.com>
2016-02-02 17:28:05 -0500

    jasmine added

---
+++ public/test.js
@@ -0,0 +1,46 @@
+(function() {
+    var Notes = function() {
+        var notes = [];
+        this.add = function(note) {
+            notes.push(note);
+        }
+    }
+
+    //add an object that implements Notes interface is used in a service to add notes eg:
+
+    var NotesService = function(notes) {
+
+        function writeNote(msg) {
+            notes.add(msg);
+        }
+
+        return {
+            writeNote: writeNote
+        }
+    }
+
+    //complete the unit test to verify that Notes.add has been invoked to add notes
+    describe('Notes Service', function() {
+        var notesService, notes;
+
+        beforeEach(function() {
+            notes = new Notes();
+            notesService = new NotesService(notes);
+        })
+
+        it('should use Noets API to add notes', function() {
+            // decorator, make sure the raw function is called as well.
+            var add = notes.add;
+            var called = false;
+            // can switch with jasmine spy on
+            // http://jasmine.github.io/2.0/introduction.html
+            // code will be like spyOn(notes, 'add')
+            notes.add = function(msg) {
+                called = true;
+                add.call(notes, msg);
+            }
+            notesService.writeNote('a')
+            expect(called).toEqual(true);
+        });
+    });
+})();
